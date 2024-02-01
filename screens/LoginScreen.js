3a9fcd853822d9bd5f49bcd22272c5e01a0aa35d
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2024-02-01 20:26:45 -0300

    login screen half complete, stack navigator made.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,111 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <View>
+        <Image
+          style={{ width: 150, height: 100 }}
+          source={{
+            uri: "https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png",
+          }}
+        />
+      </View>
+
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: "bold",
+              marginTop: 12,
+              color: "#041e42",
+            }}
+          >
+            Faça seu Login na Amazon
+          </Text>
+        </View>
+      </KeyboardAvoidingView>
+
+      <View style={{ marginTop: 70 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            backgroundColor: "#D0D0D0",
+            paddingVertical: 5,
+            borderRadius: 5,
+            marginTop: 30,
+          }}
+        >
+          <MaterialIcons
+            style={{ marginLeft: 8 }}
+            name="email"
+            size={24}
+            color={"gray"}
+          />
+          <TextInput
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={{
+              color: "gray",
+              marginVertical: 10,
+              width: 300,
+              fontSize: email ? 16 : 16,
+            }}
+            placeholder="Insira seu Email"
+          />
+        </View>
+
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="key"
+              size={24}
+              color={"gray"}
+            />
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: password ? 16 : 16,
+              }}
+              placeholder="Insira sua senha"
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
