71a7a4b5fb1d7156298df0504ef521f2c31dc5e7
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2024-02-02 03:36:45 -0300

    finalizada estilização tela de login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,13 +6,16 @@ import {
   Image,
   KeyboardAvoidingView,
   TextInput,
+  Pressable,
 } from "react-native";
 import React, { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigation = useNavigation();
   return (
     <SafeAreaView
       style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
@@ -103,6 +106,53 @@ const LoginScreen = () => {
               placeholder="Insira sua senha"
             />
           </View>
+
+          <View
+            style={{
+              marginTop: 12,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text>Me mantenha conectado</Text>
+            <Text style={{ color: "#007FFF", fontWeight: 500 }}>
+              Esqueci minha senha
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 50 }} />
+
+          <Pressable
+            style={{
+              width: 200,
+              backgroundColor: "#FEBE10",
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+              padding: 15,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              Login
+            </Text>
+          </Pressable>
+
+          <Pressable
+            style={{ marginTop: 15 }}
+            onPress={() => navigation.navigate("Register")}
+          >
+            <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+              Novo usuário? Cadastre-se
+            </Text>
+          </Pressable>
         </View>
       </View>
     </SafeAreaView>
