ed4f285b6a620c48dc1499c70404d4a78e24737f
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2024-02-09 23:17:02 -0300

    feat: inicio tela de adicionar endereço o testes do componente header

--- screens/AddAddressScreen.js
+++ screens/AddAddressScreen.js
@@ -1,6 +1,7 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Pressable } from "react-native";
 import React from "react";
 import Header from "../components/Header";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const AddAddressScreen = () => {
   return (
@@ -9,6 +10,13 @@ const AddAddressScreen = () => {
       style={{ marginTop: 50 }}
     >
       <Header />
+      <View style={{ padding: 10 }}>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Seus Endereços</Text>
+        <Pressable>
+          <Text>Adicionar novo endereço</Text>
+          <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
+        </Pressable>
+      </View>
     </ScrollView>
   );
 };
