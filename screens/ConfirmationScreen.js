5eb2affbba59a3b25aa78c4e8e22431eb0db94dd
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2024-02-23 05:42:29 -0300

    feat: inicio de criação da screen de pagamento/metodo de entrega.

--- screens/ConfirmationScreen.js
+++ screens/ConfirmationScreen.js
@@ -1,11 +1,78 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
 
 const ConfirmationScreen = () => {
+  const steps = [
+    { title: "Address", content: "Address Form" },
+    { title: "Delivery", content: "Delivery Options" },
+    { title: "Payment", content: "Payment Details" },
+    { title: "Place Order", content: "Order Summary" },
+  ];
+  const [currentStep, setCurrentStep] = useState(0);
   return (
-    <View>
-      <Text>ConfirmationScreen</Text>
-    </View>
+    <ScrollView style={{ marginTop: 55 }}>
+      <View style={{ flex: 1, paddingHorizontal: 20, paddingTop: 40 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginBottom: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          {steps?.map((step, index) => (
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+              {index > 0 && (
+                <View
+                  style={[
+                    { flex: 1, height: 2, backgroundColor: "green" },
+                    index <= currentStep && { backgroundColor: "green" },
+                  ]}
+                />
+              )}
+              <View
+                style={[
+                  {
+                    width: 30,
+                    height: 30,
+                    borderRadius: 15,
+                    backgroundColor: "#ccc",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  },
+                  index < currentStep && { backgroundColor: "green" },
+                ]}
+              >
+                {index < currentStep ? (
+                  <Text
+                    style={{ fontSize: 16, fontWeight: "bold", color: "white" }}
+                  >
+                    &#10003;
+                  </Text>
+                ) : (
+                  <Text
+                    style={{ fontSize: 16, fontWeight: "bold", color: "white" }}
+                  >
+                    {index + 1}
+                  </Text>
+                )}
+              </View>
+              <Text style={{ textAlign: "center", marginTop: 8 }}>
+                {step.title}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </View>
+
+      {currentStep == 0 && (
+        <View style={{ marginHorizontal: 20 }}>
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+            Selecione o endereço de entrega
+          </Text>
+        </View>
+      )}
+    </ScrollView>
   );
 };
 
